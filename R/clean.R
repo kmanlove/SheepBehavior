9fecc9490c22f9836e0501b0c3695b6dc2ace78f
Kezia Manlove <kezia.manlove@gmail.com>
2015-04-08 12:09:38 -0600

    add TimePosix fields to scans data in clean.R

--- R/clean.R
+++ R/clean.R
@@ -5,3 +5,6 @@
 source("./R/StaticNetworkAssocMat.R")
 
 # clean scans
+scans$TimePosix0 <- strptime(scans$Time, "%H:%M:%S %p", tz = "")
+scans$TimePosix <- as.numeric(strptime(scans$Time, "%I:%M:%S %p", tz = ""))
+scans$TimePosix2 <- as.numeric(strptime(scans$Time, "%l"))
