cf76d89f2baa064a7caa885a95e2fe37e804c8c3
Kezia Manlove <kezia.manlove@gmail.com>
2015-04-08 12:17:10 -0600

    source and run CleanRawScans in clean.R

--- R/clean.R
+++ R/clean.R
@@ -1,10 +1,18 @@
 # cleaning
 
-# source functions to covert relocations to associations
+# source cleaning functions
 
+# relocations to associations
 source("./R/StaticNetworkAssocMat.R")
 
+# scans
+source("./R/CleanRawScans.R")
+
 # clean scans
+# add time fields to raw scans
 scans$TimePosix0 <- strptime(scans$Time, "%H:%M:%S %p", tz = "")
 scans$TimePosix <- as.numeric(strptime(scans$Time, "%I:%M:%S %p", tz = ""))
 scans$TimePosix2 <- as.numeric(strptime(scans$Time, "%l"))
+
+# convert raw scans to individual compiled scans
+clean.scans <- CleanRawScans(scans)
