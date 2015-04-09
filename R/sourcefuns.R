2810b791ad970afca5421d39c75cf419f0fe00ea
Kezia Manlove <kezia.manlove@gmail.com>
2015-04-09 08:10:35 -0600

    add StaticNetworkAssocMat to sourcefuns.R

--- R/sourcefuns.R
+++ R/sourcefuns.R
@@ -1,6 +1,7 @@
 # script that sources all required functions
 
 source("./R/NetworkPlottingSourceFunctions.R")
+source("./R/StaticNetworkAssocMat.R")
 
 # plotting functions
 source("./R/PlotScanTimeByDateByPop.R")
